a7749a3f266a5fe6eec517f9171f8d74ca6efc52
yuShaf <yury.shafran@gmail.com>
2018-05-06 15:53:08 +0300

    В полноэкранное activity с помощью Glide по переданной через Intent ссылке загружается изображение.

--- app/src/main/java/com/yushaf/daygallery/ImageActivity.java
+++ app/src/main/java/com/yushaf/daygallery/ImageActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
 import org.w3c.dom.Text;
 
 /**
@@ -16,17 +18,6 @@ import org.w3c.dom.Text;
  * status bar and navigation/system bar) with user interaction.
  */
 public class ImageActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -52,7 +43,6 @@ public class ImageActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
-    private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -61,7 +51,6 @@ public class ImageActivity extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
         }
     };
     private boolean mVisible;
@@ -71,31 +60,14 @@ public class ImageActivity extends AppCompatActivity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_image);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
+        mContentView = findViewById(R.id.image);
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -105,17 +77,14 @@ public class ImageActivity extends AppCompatActivity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
-
         Intent intent = getIntent();
         if (intent != null) {
             String url = intent.getStringExtra(getString(R.string.intentDataKey));
-            TextView textView = findViewById(R.id.fullscreen_content);
-            textView.setText(url);
+            ImageView image = findViewById(R.id.image);
+            Glide
+                    .with(this)
+                    .load(url)
+                    .into(image);
         }
     }
 
@@ -139,11 +108,7 @@ public class ImageActivity extends AppCompatActivity {
 
     private void hide() {
         // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
+        mShowPart2Runnable.run();
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
